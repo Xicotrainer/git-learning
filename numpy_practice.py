4cfbf89f1b4ed276f2993b400a7eeed932fe5a90
Xicotrainer <xicotrainer@gmail.com>
2022-01-29 18:19:15 -0600

    The project was change as a numpy practice, now th e main file (numpy_practice.py) runs

--- numpy_practice.py
+++ numpy_practice.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sb
+
 
 def main():
-    lst = [x+1 for x in range(89) if x%11 = 1]
+    lst = [x+1 for x in range(89) if x%11 == 1]
     for l in list:
         print("This is the sequence: {}".format(l))
