d1729afc83155ed1d6ac655a39135fd63e8d9303
Xicotrainer <xicotrainer@gmail.com>
2022-01-29 21:37:32 -0600

    This is the first wrll-tracket commit because I was do it with 'git add *' instead 'git add .'

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log('Qué tranza la banda');
--- numpy_practice.py
+++ numpy_practice.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-def main():
+def run():
     lst = [x+1 for x in range(89) if x%11 == 1]
-    for l in list:
-        print("This is the sequence: {}".format(l))
+    for l in lst:
+        print("This is the sequence for all numbers under 89 with module 11: {}".format(l))
+
+if __name__ == "__main__":
+    run()
